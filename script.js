8c79d5db6c1b60603f18792ff2c365dee980d027
kawodyaarchchige <kawodya.wa@gmail.com>
2024-04-14 03:44:03 +0530

    changed the design

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function toggleNav() {
     var navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
 }
+document.addEventListener("DOMContentLoaded", function() {
+    var landingPage = document.querySelector('.landing-page');
+    var mainContent = document.querySelector('main');
+
+    // Detect the end of the animation and show the main content
+    landingPage.addEventListener('animationend', function() {
+        showMainContent();
+    });
+
+    function showMainContent() {
+        // Hide the landing page and display the main content
+        landingPage.style.display = 'none';
+        mainContent.style.display = 'block';
+    }
+});
