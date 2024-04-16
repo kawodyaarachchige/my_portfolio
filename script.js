d1d2513a78a20aa6bc3abd0d69beb52c89c79dc6
kawodyaarchchige <kawodya.wa@gmail.com>
2024-04-17 03:17:18 +0530

    fixed some responsive issues in home n about sections

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-function toggleNav() {
-    var navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
+document.addEventListener("DOMContentLoaded", function () {
+    const hamburgerMenu = document.querySelector(".hamburger-menu");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburgerMenu.addEventListener("click", function () {
+        hamburgerMenu.classList.toggle("active");
+        navLinks.classList.toggle("active");
+    });
+
+    // Add an event listener to each navigation link to hide the menu when a link is clicked
+    const navItems = document.querySelectorAll('.nav-links li a');
+    navItems.forEach(function(item) {
+        item.addEventListener('click', function() {
+            hamburgerMenu.classList.remove("active");
+            navLinks.classList.remove("active");
+        });
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     var landingPage = document.querySelector('.landing-page');
     var mainContent = document.querySelector('main');
@@ -17,3 +32,10 @@ document.addEventListener("DOMContentLoaded", function() {
         mainContent.style.display = 'block';
     }
 });
+const cursor = document.querySelector("#cursor");
+document.addEventListener("mousemove", function (e){
+    window.requestAnimationFrame(() => {
+        cursor.style.top = `${e.clientY - cursor.offsetHeight/2}px`;
+        cursor.style.left = `${e.clientX - cursor.offsetWidth/2}px`;
+    });
+})
